1a28b6802b73d6a349db0ead58e6c511b702884f
Aayush Dhariwal <dhariwalaayush04@gmail.com>
2024-10-06 19:01:36 +0530

    Build Timer
    Added a build method to build tower child by child with some delay

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float buildDelay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
+
     public bool CreateTower(Tower tower, Vector3 position)              //To spawn the tower where mouse is being clicked
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -21,5 +28,27 @@ public class Tower : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    IEnumerator Build()         // For build timer, tower will take sometime to build child by child
+    {
+        foreach (Transform child in transform)        //Disabling all children
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);            
+            }
+        }
+
+        foreach (Transform child in transform)         //Enabling all children with buildDelay wait 
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
